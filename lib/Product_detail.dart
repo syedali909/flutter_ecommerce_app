06372611893ed7d31dd82f38578ffac9af38c48b
syedali909@gmail.com <Syed@8004>
2019-10-10 17:52:08 +0530

    5rd Commit

--- lib/Product_detail.dart
+++ lib/Product_detail.dart
@@ -125,7 +125,7 @@ class _ProductDetailState extends State<ProductDetail> {
                   onPressed: (){},
                   color: Colors.red,
                   textColor: Colors.white,
-                  elevation: 0.2,
+                  elevation: 0.3,
                   child: new Text("Buy now "),
                 ),
               ),
